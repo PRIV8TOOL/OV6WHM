9f6056b22e5e75128dacc96d213dde90230eb54b
W3LLL <81862344+W3LLLL@users.noreply.github.com>
2022-09-13 11:36:24 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+yum update -y
+clear
+printf "\e[92mEnter Your hostname (example: test.anything.com): \033[0;33m"
+read hostnya
+hostnamectl set-hostname $hostnya
+systemctl list-unit-files | grep NetworkManager
+curl -o latest -L http://httpupdate.cpanel.net/latest && sh latest
+printf '\e[0mYou can access WHM by visiting \e[0;32mhttp://'$(hostname -I | cut -d' ' -f1)'/whm\n'
